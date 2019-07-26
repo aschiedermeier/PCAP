3b9bdee9c6631277365374872c93e7a7b39323cf
aschiedermeier <aschiedermeier@users.noreply.github.com>
2019-07-26 22:00:38 +0200

    updated some answers

--- FreeSampleTest.py
+++ FreeSampleTest.py
@@ -9,6 +9,17 @@
 print("""\n#1: from right to left. 2**9 = 512""")
 
 print("""\n#2: usage of \\, \" and \' """)
+a=b=c=d = None
+
+a="Peter's sister's name's \"Anna\""
+b='Peter\'s sister\'s name\'s \"Anna\"'
+#c= "Peter's sister's name's "Anna""
+#d= 'Peter's sister's name's "Anna"'
+
+print (a)
+print (b)
+print (c)
+print (d)
 
 print("""\n#3: lenght of string(i) is 3, so we go else-path. """)
 
@@ -25,15 +36,62 @@ print("""\n#8: b = 1 --> c = 2 """)
 print("""\n#9: A: 3 stars. 
     No error, the else is ok. 3.1.2.16
     The while and for loops can also have an else clause in Python. 
-    The else clause executes after the loop finishes its execution as long as it has not been terminated by break""")
+    The else clause executes after the loop finishes its execution as long as it has not been terminated by break
+    In this case: 3 starts in while loop, then break, so no else path""")
 
-print("""\n#10: all have only one line, as end command deletes the break line command.""")
-for i in range (1,4,2):
-    print("*",end="*")
-print("**")
+i = 10
+while i > 0 :
+    i -= 3
+    print("*")
+    if i <= 3:
+        break
+else:
+    print("*")
 
-print("""\n#11: D: The Python Language Reference is the official reference manual that
+print("""\n#10: ex1: no end --> break line command
+		ex 2-4 have only one line, as end command deletes the break line command.""")
+# Example 1
+for i in range(1, 4, 2):
+    print("*")
+print()
+
+# Example 2
+for i in range(1, 4, 2):
+    print("*", end="")
+print()
+
+# Example 3
+for i in range(1, 4, 2):
+    print("*", end="**")
+
+print()
+
+# Example 4
+for i in range(1, 4, 2):
+    print("*", end="**")
+print("***")
+
+
+print("""\n#11: 
+A: Unicode assigns unique (unambiguous) characters (letters, hyphens, ideograms, etc.) to more than a million code points. 
+The first 128 Unicode code points are identical to ASCII, 
+and the first 256 Unicode code points are identical to the ISO/IEC 8859-1 code page (a code page designed for western European languages).
+
+B: One of the most commonly used is UTF-8.
+The name is derived from Unicode Transformation Format.
+The concept is very smart. UTF-8 uses as many bits for each of the code points as it really needs to represent them.
+
+C: The system named ASCII (short for American Standard Code for Information Interchange) is the most widely used, 
+and you can assume that nearly all modern devices (like computers, printers, mobile phones, tablets, etc.) use that code.
+The code provides space for 256 different characters, but we are interested only in the first 128. 
+
+D: The Python Language Reference is the official reference manual that
 describes the syntax and semantics of the Python language.
+
+E,F:First of all, Python's strings (or simply strings, as we're not going to discuss any other language's strings) are immutable sequences.
+Strings aren't lists, but you can treat them like lists in many particular cases.
+Moreover, everything you know about slices is still usable.
+
 G: Lists and strings in Python can be sliced.
 """)
 
@@ -47,9 +105,17 @@ x= "8"
 y= "30"
 print(x>y)
 
-print("""\n#13: A, ifend  slice goes beyond the range of strings no problem(error).
-    Whole string gets printed""")
+print("""\n#13: A, if end  slice goes beyond the range of strings no problem (no error).
+    Whole string gets printed.
+    If i print the single items on the borders, i get error message.""")
+s = "Hello, Python!"
 
+print(s[-14:15])
+# print(s[-15]) #error
+print(s[-14])
+print(s[13])
+#print(s[14]) # error
+print(s[-15:16]) # no error
 print("""\n#14: deleted first 3 items""")
 
 print("""\n#15: A - for item in dict: print item - returns only key. 4.1.6.11
@@ -58,12 +124,20 @@ dict = { 'a': 1, 'b': 2, 'c': 3 }
 for item in dict:
     print(item)
 
+# ame result
+for key in dict.keys():
+    print(key)
+
 # alternative code
 for value in dict.values():
     print(value)
 
 for key,value in dict.items():
     print(key+": "+str(value))
+    
+# also working, but ugly
+for item  in dict.items():
+    print(item)
 
 print("""\n#16: what happens in for loop stays in for loop
     unless it gets printed out or returned""")
@@ -110,7 +184,8 @@ for x in lst:
         if y < 2:
             print('*',end='')
 
-print("""\n#19: C, last item of list. no need to calcualate that 2**10 is 1024.""")
+print("""\n#19: C, last item of list. no need to calcualate that 2**10 is 1024.
+(But i should know those numbers by now anyway)""")
 lst = [2 ** x for x in range(0, 11)]
 print(lst[-1])
 
@@ -154,11 +229,37 @@ from math import pi as xyz # line 01
 # correct code
 print(xyz)
 
-print("""\n#24: """)
+print("""\n#24: BC
+The content of the __init__.py file is executed when any of the package's modules is imported. 
+If you don't want any special initializations, you can leave the file empty, but you mustn't omit it.
 
-print("""\n#25: """)
+B. It can execute an initialization code for a package
+C. It is required to make Python treat a given directory as a Python
+package directory
+""")
 
-print("""\n#26: """)
+
+print("""\n#25: F, i need to call the function, not the module. If corrected, then Answer A""")
+#corrected code
+from random import randint
+for i in range(10):
+ print(randint(1, 5))
+
+import random 
+for i in range(10):
+ print(random.randint(1, 5))
+ 
+from random import randint as ra
+for i in range(10):
+ print(ra(1, 5))
+
+print("""\n#26: A: 8""")
+x = 1 # line 1
+def a(x): # line 2
+    return 2 * x # line 3
+x = 2 + a(x) # line 4
+print(a(x)) # line 5
+
 
 print("""\n#27: G - error due to missing positional argument - 
     Exception gets raised in line G, where the program prints and calls the function.
@@ -175,11 +276,21 @@ print("""\n#27: G - error due to missing positional argument -
 #  return z # line 4
 # #print(x(a)) # line 5
 
-print("""\n#28: """)
+print("""\n#28: D""")
+s = 'SPAM'
+def f(x):
+    return s + 'MAPS'
+print(f(s))
 
-print("""\n#29: """)
 
-print("""\n#30: """)
+print("""\n#29: BD
+A. Positional arguments are also called keyword arguments
+B. The order of arguments matters when they are passed positionally
+C. The order of arguments matters when they are passed by their name
+D. A function can be called with a mix of positional and keyword arguments
+""")
+
+print("""\n#30: B""")
 def gen():
     lst = range(5)
     for i in lst:
@@ -190,6 +301,9 @@ for i in gen():
 print("""\n#31: ACD
     B:Some objects contain references to other objects; these are called containers. 
     Examples of containers are tuples, lists and dictionaries. The references are part of a container’s value.
+    D: The act of creating an object of the selected class is also called an instantiation 
+    (as the object becomes an instance of the class).
+    Can be made also from simple class without constructor.
     E: There is no object variables in Python, only instance variables.
     """)
 
@@ -197,6 +311,10 @@ print("""\n#32: ACD
     B: issubclass(class1, class2) is an example of a function that returns
         True if class2 is a subclass of class1 
         --> wrong order: True if class1 is subcalss of class2
+    D: The situation in which the subclass is able to modify its superclass behavior 
+    (just like in the example) is called polymorphism. 
+    polymorphism helps the developer to keep the code clean and consistent.
+    Polymorphism is a fancy word that just means the same function is defined on objects of different types.
 """)
 
 print("""\n#33: CD
